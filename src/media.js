6113f14a49d8cbdd01e8b775922e8f6db0324274
eline-create <eline.geo@gmail.com>
2021-04-15 21:15:16 -0300

    Fix: cálculo

--- src/media.js
+++ src/media.js
@@ -1,6 +1,6 @@
-function media(numero1, numero2, numero3) {
-    return (numero1 + numero2 + numero3)/3;
-  }
+function media (numero1, numero2, numero3) {
+  return (numero1 + numero2 + numero3)/3;
+}
   
     
   module.exports = {media};
--- test/media.spec.js
+++ test/media.spec.js
@@ -1,8 +1,7 @@
 const media = require('../src/media');
 
-test('a média tem que ser igual 7', () => {
-	const resultado = media(10,9,8);
+test('a média entre 10, 9 e 8 tem que ser igual 9', () => {
+	const resultado = calculadora.media(10, 9, 8);
 
-	expect(resultado).toEqual(7);
+	expect(resultado).toEqual(9);
 });
-
